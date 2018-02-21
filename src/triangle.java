2e3fb3d33658acde212622e9fd9bd0d197fd544c
ZayldeCurt <michaswiatowy@gmail.com>
2018-02-21 20:16:38 +0100

    nowe

--- src/triangle.java
+++ src/triangle.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class triangle {
 
     public triangle(double bokA, double bokB, double bokC) {
@@ -20,21 +22,96 @@ public class triangle {
     public boolean defaultBokA;
     public boolean defaultBokB;
     public boolean defaultBokC;
+    public boolean defaultBok;
+
 
     public double bokA;
     public double bokB;
     public double bokC;
     public double Obwod;
 
+
+    void CheckDefault()
+    {
+        if(!defaultBokA||!defaultBokB || !defaultBokC)
+        {
+            defaultBok=false;
+            System.out.println("nie domyslne");
+        }
+        else
+        {
+            System.out.println("domyslne");
+        }
+    }
+
+    public void PobierzBok()
+    {
+        int ilosc;
+        System.out.println("ile bokow chcesz wpisac?");
+        Scanner odczyt = new Scanner(System.in);
+        odczyt = new Scanner(System.in);
+        while(!odczyt.hasNextInt())
+        {
+            System.out.println("zle, jeszcze raz:");
+
+        }
+        ilosc=odczyt.nextInt();
+        if (ilosc>3)
+        {
+            System.out.println("trojkat moze miec tylko trzy boki");
+            ilosc=3;
+        }
+
+
+        System.out.println("podaj bok trójkąta");
+
+
+        while(!odczyt.hasNextDouble())
+        {
+            System.out.println("zle, jeszcze raz:");
+            //odczyt = new Scanner(System.in);
+        }
+        this.bokA = odczyt.nextDouble();
+        this.defaultBokA=false;
+        if(ilosc>=2)
+        {
+            System.out.println("podaj drugi bok trójkąta");
+
+
+            while(!odczyt.hasNextDouble())
+            {
+                System.out.println("zle, jeszcze raz:");
+
+            }
+            this.bokB = odczyt.nextDouble();
+            this.defaultBokB=false;
+            if(ilosc==3)
+            {
+                System.out.println("podaj trzeci bok trójkąta");
+
+
+                while(!odczyt.hasNextDouble())
+                {
+                    System.out.println("zle, jeszcze raz:");
+
+                }
+                this.bokC = odczyt.nextDouble();
+                this.defaultBokC=false;
+            }
+        }
+
+    }
+
     void oblicz_przeciwprostokatna()
     {
         this.bokC = Math.sqrt(Math.pow(bokA,2)+Math.pow(bokB,2));
+        defaultBokC=false;
     }
     void oblicz_obwod()
     {
         if(defaultBokC)
         {
-            defaultBokC=false;
+
             oblicz_przeciwprostokatna();
         }
         this.Obwod=bokA+bokB+bokC;
--- src/triangle_ui.java
+++ src/triangle_ui.java
@@ -5,51 +5,23 @@ public class triangle_ui {
 
     public static void main(String[] args) {
 
-        triangle trojkat = new triangle(3,4,2);
+        triangle trojkat = new triangle();
         double[] boki = new double[2];
 
-//        boki =pobierzWartosci2();
-//        trojkat.bokA=boki[0];
-//        trojkat.bokB=boki[1];
 
- //       trojkat.oblicz_przeciwprostokatna();
- //       trojkat.oblicz_obwod();
+        trojkat.PobierzBok();
 
-        if(trojkat.Sprawdz_Czy_Prostokatny())
+        trojkat.CheckDefault();
+
+
+ /*       if(trojkat.Sprawdz_Czy_Prostokatny())
         {
             System.out.println("jest prostokatny");
         }
         else
         {
-            System.out.println("nie jest");
+            System.out.println("nie jest prostokatny");
         }
-
-    }
-
-    static double[] pobierzWartosci2()
-    {
-        double[] wynik = new double[2];
-
-        System.out.println("podaj pierwszy bok trójkąta");
-        Scanner odczyt = new Scanner(System.in);
-
-        while(!odczyt.hasNextDouble())
-        {
-            System.out.println("zle, jeszcze raz:");
-            odczyt = new Scanner(System.in);
-        }
-        wynik[0] = odczyt.nextDouble();
-
-        System.out.println("podaj drugi bok trójkąta");
-
-        odczyt = new Scanner(System.in);
-        while(!odczyt.hasNextDouble())
-        {
-            System.out.println("zle, jeszcze raz:");
-            odczyt = new Scanner(System.in);
-        }
-        wynik[1] = odczyt.nextDouble();
-
-        return wynik;
+*/
     }
 }
