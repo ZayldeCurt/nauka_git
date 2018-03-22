464fbb8e666470fa44639d20fe25c3698c4a816d
ZayldeCurt <michaswiatowy@gmail.com>
2018-03-23 00:10:51 +0100

    sort bubble and insert #2

--- src/blok2PodstawyAlgorytmow/sortowania/BubbleSort.java
+++ src/blok2PodstawyAlgorytmow/sortowania/BubbleSort.java
@@ -8,7 +8,8 @@ public class BubbleSort {
     public static void main(String[] args) {
 
         Integer[] tab = {1,10,3,4,9,8,7,6,5};
-        bubbleSort(tab,false);
+//        bubbleSort(tab,false);
+        bubbleSortSecond(tab);
         System.out.println(Arrays.asList(tab));
     }
 
@@ -40,15 +41,19 @@ public class BubbleSort {
         }
 
 
-//        for (int i = 0; i < sizeTab-1; i++) {
-//            temp1=tab[i];
-//            temp2=tab[i+1];
-//            if (temp1 > temp2) {
-//                tab[i] = temp2;
-//                tab[i + 1] = temp1;
-//            }
-//        }
+
     }
 
+    public static void bubbleSortSecond(Integer tab[]){
+        for (int i = 0; i <tab.length-1; i++) {
+            for (int j = 0; j < tab.length-1; j++) {
+                int temp = tab[j];
+                if (temp < tab[j + 1]) {
+                    tab[j] = tab[j + 1];
+                    tab[j + 1] = temp;
+                }
+            }
+        }
+    }
 
 }
--- src/blok2PodstawyAlgorytmow/sortowania/InsertSort.java
+++ src/blok2PodstawyAlgorytmow/sortowania/InsertSort.java
@@ -5,8 +5,11 @@ import java.util.Arrays;
 public class InsertSort {
 
     public static void main(String[] args) {
-        Integer[] tab = {8,5,4,9,2,6};
-//        insertSortWhile(tab);
+//        Integer[] tab = {8,5,4,9,2,6};
+        Integer[] tab = {7,3,8,5,2};
+
+        System.out.println(Arrays.asList(tab));
+        insertSortSecond(tab);
         System.out.println(Arrays.asList(tab));
 
     }
@@ -22,20 +25,49 @@ public class InsertSort {
             }
         }
     }
-//    private static void insertSortWhile(Integer[] tab) {
-//        for(int i = 0; i < tab.length; i++) {
-//            int j=i;
-//            System.out.println(Arrays.asList(tab));
-//            while(j>=0 &&  tab[j] < tab[j + 1] ) {
-//
-//                int temp = tab[j + 1];
-//                tab[j + 1] = tab[j];
-//                tab[j] = temp;
-//                j--;
-//                System.out.println(Arrays.asList(tab));
-//            }
-//
-//        }
-//    }
+    public static void insertSortString(String[] array) {
+        for (int i = 1; i < array.length; i++) {
+            for (int j = i; j > 0; j--) {
+                char[] chars = array[j].toLowerCase().toCharArray();
+                char[] chars2 = array[j - 1].toLowerCase().toCharArray();
+                int k = 0;
+                do {
+                    if (chars[k] < chars2[k]) {
+                        String temp = array[j - 1];
+                        array[j - 1] = array[j];
+                        array[j] = temp;
+                        break;
+                    } else if (chars[k] == chars2[k]) {
+                        if (k == Math.min(chars.length, chars2.length) -1 && chars.length<chars2.length) {
+                            String temp = array[j - 1];
+                            array[j - 1] = array[j];
+                            array[j] = temp;
+                            break;
+                        }
+                        k++;
+                    } else {
+                        break;
+                    }
+                }
+                while (k < Math.min(chars.length, chars2.length));
+            }
+        }
+    }
+
+    public static void insertSortSecond(Integer[] tab){
+        int size = tab.length;
+        int temp ;
+        for (int i = size-2; i >=0; i--) {
+            temp=tab[i];
+            int j=i;
+            while(j<size-1&&temp>tab[j+1]){
+                tab[j]=tab[j+1];
+                tab[j+1]=temp;
+                j++;
+            }
+        }
+
+
+    }
 
 }
